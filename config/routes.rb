4d1dfdb4a3f6f8230ab5eb973b71c3a2000b400a
lfb1206 <lucas.fernndez@uc.cl>
2022-04-26 11:10:25 -0400

    Avance devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+    },
+    path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout', sign_up: 'register'
+    }
+  devise_for :usuarios
   root 'articles#index'
 
   get '/articles', to: 'articles#index'
 end
+
