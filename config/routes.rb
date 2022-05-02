aae1cef1c3be7952a53dd9e2e3f7bf82dc2d184f
Vicente <vcl@uc.cl>
2022-05-01 20:57:21 -0400

    cambios turnos

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'articles#index'
 
+  #### INICIO
+  root 'articles#index'
   get '/articles', to: 'articles#index'
+
+  #### CREATE
+  get 'turnos/new'
+  post 'turnos', to: 'turnos#create'
+
+  #### READ
+  get 'turnos/index'
+  get 'turnos/show'
+
+  #### UPDATE
+  get 'turnos/edit'
+  patch 'turnos/update', to: 'turnos#update', as: 'turnos_update'
+
+  #### DELETE
+  delete 'turnos/delete', to: 'turnos#delete', as: 'turnos_delete'
+
 end
