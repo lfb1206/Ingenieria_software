726f32430a18d03e54385ef192451623ae711512
ANTONIA BLANCO <antonia.blanco@uc.cl>
2022-05-03 11:35:41 -0400

    Se agrega funcionalmente CRUD request

--- db/migrate/20220502235819_create_requests.rb
+++ db/migrate/20220502235819_create_requests.rb
@@ -1,10 +1,10 @@
 class CreateRequests < ActiveRecord::Migration[6.0]
   def change
     create_table :requests do |t|
-      t.integer, :id_solicitud
-      t.integer, :id_usuario
-      t.integer, :id_publicacion
-      t.string, :estado
+      t.integer :id_solicitud
+      t.integer :id_usuario
+      t.integer :id_publicacion
+      t.string :estado
       t.text :descripcion
 
       t.timestamps
