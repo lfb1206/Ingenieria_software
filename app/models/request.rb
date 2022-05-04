a84686c0be4dbc7ccf00b18aa1e649fc1860b20e
lfb1206 <lucas.fernndez@uc.cl>
2022-05-04 12:46:14 -0400

    Validates terminados

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 
 class Request < ApplicationRecord
-  validates :name, length: { minimum: 2 }
-  validates :description, length: { minimum: 5 }
-  validates :email, presence: true, uniqueness: true
-  validates :age, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/turno.rb
+++ app/models/turno.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class Turno < ApplicationRecord
-  validates :name, length: { minimum: 2 }
-  validates :description, length: { minimum: 5 }
-  validates :email, presence: true, uniqueness: true
-  validates :age, numericality: { greater_than_or_equal_to: 0 }
-  validates :cantidad_asientos, presence: true, length: { minimum: 1, maximum: 1 }
+  validates :cantidad_asientos, presence: true, comparison: { greater_than_or_equal_to: 1,less_than_or_equal_to: 4}, numericality: { only_integer: true }
   validates :direccion_salida, presence: true
   validates :direccion_llegada, presence: true
   validates :dia_semana, presence: true, length: { minimum: 5, maximum: 9 }
