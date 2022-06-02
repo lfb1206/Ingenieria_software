66535a525dde920bd53f9a466923e31007842839
lfb1206 <lucas.fernndez@uc.cl>
2022-06-01 22:01:35 -0400

    Foto agregada a crud, modelo, migracion, mensajes y show de cuenta

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -35,12 +35,12 @@ module Users
 
     # If you have extra params to permit, append them to the sanitizer.
     def configure_sign_up_params
-      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name address description gender phone])
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name address description gender phone foto])
     end
 
     # If you have extra params to permit, append them to the sanitizer.
     def configure_account_update_params
-      devise_parameter_sanitizer.permit(:account_update, keys: %i[name address description gender phone])
+      devise_parameter_sanitizer.permit(:account_update, keys: %i[name address description gender phone foto])
     end
 
     # The path used after sign up.
